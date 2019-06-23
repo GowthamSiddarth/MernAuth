da19507e8f5ceed3a2d98d2db2973b1ac5c81d0a
Gowtham Behara <gothsiddu@gmail.com>
2019-06-23 19:10:33 +0530

    Throw validation failure if field values are empty

--- validation/login.js
+++ validation/login.js
@@ -7,11 +7,11 @@ module.exports = function validateLoginInput(data) {
     data.email = isEmpty(data.email) ? "" : data.email
     data.password = isEmpty(data.password) ? "" : data.password
 
-    if (!validator.isEmpty(data.email)) {
+    if (validator.isEmpty(data.email)) {
         errors.email = "Email field is required"
     }
 
-    if (!validator.isEmpty(data.password)) {
+    if (validator.isEmpty(data.password)) {
         errors.password = "Password field is required"
     }
 
