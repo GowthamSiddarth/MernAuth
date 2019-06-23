65fb8e87b9c69f61a7a5e0baf183b4213d975be4
Gowtham Behara <gothsiddu@gmail.com>
2019-06-23 19:01:44 +0530

    Create route for login and check user login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,9 +16,9 @@ router.post("/register", (req, res) => {
         return res.status(400).json(errors)
     }
 
-    User.findOne({email: req.body.email}).then(user => {
+    User.findOne({ email: req.body.email }).then(user => {
         if (user) {
-            return res.status(400).json({email: "Email already exists"})
+            return res.status(400).json({ email: "Email already exists" })
         } else {
             const newUser = new User({
                 name: req.body.name,
@@ -38,3 +38,42 @@ router.post("/register", (req, res) => {
         }
     })
 })
+
+router.post("/login", (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body)
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+
+    const email = req.body.email
+    const password = req.body.password
+
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            res.status(404).json({ email_not_found: "Email not found" })
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                }
+
+                jwt.sign(payload, keys.secretOrKey, {
+                    expiresIn: 31556926
+                }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    })
+                })
+            } else {
+                return res.status(400).json({ password_incorrect: "Password incorrect" })
+            }
+        })
+    })
+})
+
+module.exports = router
