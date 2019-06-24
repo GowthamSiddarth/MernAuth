f1bf7165eb32e6268895e6f243a4d9fa8203a01a
Gowtham Behara <gothsiddu@gmail.com>
2019-06-24 12:44:40 +0530

    Remove semi-colons

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const users = require("./routes/api/users")
 const app = express()
 
 // Parse application/x-www-form-urlencoded requests
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Parse application/json requests
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-const db = require("./config/keys").mongoUrl;
+const db = require("./config/keys").mongoUrl
 
 mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log("DB connected successfully"))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 
 app.use(passport.initialize())
 require('./config/passport')(passport)
