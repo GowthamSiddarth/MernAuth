de6212cfa6830448a32d6fd118a753f4412c80f6
Gowtham Behara <gothsiddu@gmail.com>
2019-06-24 15:25:14 +0530

    Add components for div

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <Landing />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
