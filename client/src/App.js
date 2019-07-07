507d47bdf133b498f1aaefe234f77f7567a83f31
Gowtham Behara <gothsiddu@gmail.com>
2019-07-07 16:14:04 +0530

    Handle events for Register Component; Route Register through App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Register from './auth/Register'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+      </div>
+    </BrowserRouter>
   )
 }
 
