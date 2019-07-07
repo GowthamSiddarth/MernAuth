fb50e2883fa49b286a4c2ecce54a2569e1b0ea1b
Gowtham Behara <gothsiddu@gmail.com>
2019-07-07 16:34:48 +0530

    Create component for Login and Add to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Register from './auth/Register'
+import Login from './auth/Login'
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <Navbar />
         <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
