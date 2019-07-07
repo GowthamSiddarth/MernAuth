dff101f6cf7af6e517a4c6f3495719ec7ec5bb42
Gowtham Behara <gothsiddu@gmail.com>
2019-07-07 16:34:14 +0530

    Remove BrowserRoute enclosing Links and reformat code

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, BrowserRouter } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function Landing() {
     return (
@@ -17,34 +17,30 @@ function Landing() {
                     </p>
                     <br />
                     <div className="col s6">
-                        <BrowserRouter>
-                            <Link
-                                to="/register"
-                                style={{
-                                    width: "140px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px"
-                                }}
-                                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                            >
-                                Register
+                        <Link
+                            to="/register"
+                            style={{
+                                width: "140px",
+                                borderRadius: "3px",
+                                letterSpacing: "1.5px"
+                            }}
+                            className="btn btn-large waves-effect waves-light hoverable green accent-8"
+                        >
+                            Register
                             </Link>
-                        </BrowserRouter>
                     </div>
                     <div className="col s6">
-                        <BrowserRouter>
-                            <Link
-                                to="/login"
-                                style={{
-                                    width: "140px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px"
-                                }}
-                                className="btn btn-large btn-flat waves-effect white black-text"
-                            >
-                                Log In
+                        <Link
+                            to="/login"
+                            style={{
+                                width: "140px",
+                                borderRadius: "3px",
+                                letterSpacing: "1.5px"
+                            }}
+                            className="btn btn-large btn-flat waves-effect hoverable blue black-text"
+                        >
+                            Log In
                         </Link>
-                        </BrowserRouter>
                     </div>
                 </div>
             </div>
